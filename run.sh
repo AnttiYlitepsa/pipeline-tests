69b4bc4497fd7570aed6409a2cc644b9a70f55c2
AnttiYlitepsa <antti.ylitepsa@comiq.fi>
2018-06-24 14:23:30 +0000

    Added simple testsuite and scripts to run it

---
+++ run.sh
@@ -0,0 +1,11 @@
+ #!/bin/bash
+
+rm -r report || true
+
+docker-compose up -d xvfb
+sleep 10
+
+docker-compose up robotframework
+
+#docker run -v test-suite:/suite robot_framework
+docker-compose down
